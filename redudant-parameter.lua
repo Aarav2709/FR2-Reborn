7eb8f7d9b707379822211fe1f2c9a1485c1bb2af
Aarav Gupta <tribejustice35@gmail.com>
2026-01-19 21:46:14 +0530

    Fix over 450 Hoax Errors.

---
+++ redudant-parameter.lua
@@ -0,0 +1,1 @@
+---@diagnostic disable: redundant-parameter
---
+++ solar2d_globals.lua
@@ -0,0 +1,7 @@
+---@diagnostic disable: undefined-global
+
+display = display
+system = system
+timer = timer
+Runtime = Runtime
+network = network

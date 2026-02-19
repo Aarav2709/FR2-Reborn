f4e8659b6fadd0e2d7da4271bf2ba4acb0b81789
Aarav Gupta <tribejustice35@gmail.com>
2026-02-19 13:21:49 +0530

    Fixed Forest and Town TOO!

--- lua/map/assets/forest/specialPropertyData.lua
+++ lua/map/assets/forest/specialPropertyData.lua
@@ -1,18 +1,33 @@
-local M = {}
+--------------------------------------- 
+local L0_1, L1_1
+L0_1 = {}
 
-function M.init(theme)
-  local animatedBlockSheetFile = require("lua.map.assets." .. theme .. ".animatedTiles")
-  local blockmap = {}
-  blockmap.blocks = {
-    {
-      behavior = {
-        "lua.map.behaviors.thornBehavior"
-      },
-      noBaseImage = true
-    }
-  }
-  blockmap.frames = {special001 = 1, special002 = 1}
-  return blockmap
+function L1_1(A0_2)
+  local L1_2, L2_2, L3_2, L4_2, L5_2, L6_2
+  L1_2 = require
+  L2_2 = "lua.map.assets."
+  L3_2 = A0_2
+  L4_2 = ".animatedTiles"
+  L2_2 = L2_2 .. L3_2 .. L4_2
+  L1_2 = L1_2(L2_2)
+  L2_2 = {}
+  L3_2 = {}
+  L4_2 = {}
+  L5_2 = {}
+  L6_2 = "lua.map.behaviors.slowTileBehavior"
+  L5_2[1] = L6_2
+  L4_2.behavior = L5_2
+  L4_2.noBaseImage = true
+  L3_2[1] = L4_2
+  L2_2.blocks = L3_2
+  L3_2 = {}
+  L3_2.special001 = 1
+  L3_2.special002 = 1
+  L2_2.frames = L3_2
+  return L2_2
 end
 
-return M
+L0_1.init = L1_1
+return L0_1
+ 
+---------------------------------------

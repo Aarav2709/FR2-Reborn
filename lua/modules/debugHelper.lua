bf952acb27fada0f01f98ef8761dc62b68ee7361
Aarav Gupta <tribejustice35@gmail.com>
2026-07-05 22:14:09 +0530

    Update to duckquill template.

--- lua/modules/debugHelper.lua
+++
@@ -1,8 +0,0 @@
-local composer = require("composer")
-local showDebugPrint = false
-
-function composer.debugPrint(title, text)
-  if showDebugPrint then
-    print(title, " ", text)
-  end
-end
--- spine-lua/AttachmentType.lua
+++
@@ -1,7 +0,0 @@
-local AttachmentType = {
-  region = 0,
-  boundingbox = 1,
-  mesh = 2,
-  skinnedmesh = 3
-}
-return AttachmentType
